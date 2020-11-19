46a026c9fbf5a6903d5c9eb68e9659946c1e0add
jones <35585020+fleeced@users.noreply.github.com>
2020-11-19 11:09:53 -0700

    sidebar and topnav layout complete

--- home.php
+++ home.php
@@ -10,6 +10,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <style>
+
+    /* side navigation */
     .sidebar {
         height: 100%;
         width: 160px;
@@ -20,7 +22,7 @@
         color: #FFFFFF;
         background-color: #14171A;
         overflow-x: hidden;
-        padding-top: 16px;
+      /*  padding-top: 9px; */
     }
     .sidebar a {
         padding: 6px 8px 6px 16px;
@@ -34,26 +36,117 @@
     }
     .main {
         margin-left: 160px; /* Same as the width of the sidenav */
-        padding: 0px 10px;
+      /*  padding: 0px 10px; */
     }
     @media screen and (max-height: 450px) {
-        .sidebar {padding-top: 15px;}
+       /* .sidebar {padding-top: 15px;}*/
         .sidebar a {font-size: 18px;}
     }
+
+    /* top navigation */
+    * {box-sizing: border-box;}
+    body {
+        margin: 0;
+        }
+    .topnav {
+        overflow: hidden;
+        background-color: #14171A;
+    }/*
+    .topnav a {
+        float: left;
+        display: block;
+        color: black;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+    .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+    .topnav a.active {
+        background-color: #2196F3;
+        color: white;
+    } */
+    .topnav .search-container {
+        float: left;
+        margin-left: 170px;
+        margin-bottom: 9px; 
+        width: 200px;
+    }
+    .topnav input[type=text] {
+        padding: 6px;
+        margin-top: 8px;
+        font-size: 17px;
+        border: none;
+    }
+    .topnav button {
+        float: left;  
+        padding: 6px;
+        margin-top: 9px;
+        margin-left: 6px;
+        background: #ddd;
+        font-size: 17px;
+        color: #14171A;
+        border: none;
+        cursor: pointer;
+    }
+    .topnav .search-container button:hover {
+        background: #ccc;
+    }
+    @media screen and (max-width: 600px) {
+        .topnav .search-container {
+        float: none;
+        }
+        .topnav a, .topnav input[type=text], .topnav .search-container button {
+            float: none;
+            display: block;
+            text-align: left;
+            width: 100%;
+            margin: 0;
+            padding: 14px;
+        }
+        .topnav input[type=text] {
+            border: 1px solid #ccc;  
+        }
+    }    
+
     </style>
 
 </head>
 
 <body>
+    
+    <!-- top navigation bar -->
+    <div class="topnav">
 
+        <!-- search -->
+        <div class="search-container">
+        
+            <form method="post" id="form-search" action="twitter3.php">
+                <input type="hidden" name="page" value="HomePage">
+                <input type="hidden" name="command" value="Search">
+                <input class="form-control mr-sm-2" type="text" id="search" name="search" placeholder="Search">
+            </form>
+            
+        </div>
+
+        <button type="button" class="btn" id="search-button">Submit</button>
+        <button type="button" class="btn btn-danger" style="float:right; margin-top:9px; margin-right:10px;">Sign Out</button>
+
+    </div>
+
+    <!-- side navigation -->
     <div class="sidebar">
+
         <img src='logo.png' width='60px' height='56px' style='padding-left:10px'>
         <a href="#home">Home</a>
         <a href="#services">Profile</a>
         <a href="#clients">Notifications</a>
         <a href="#contact">Edit Profile</a>
         <br>
-        <button type="button" class="btn btn-primary" style="width:160px;">Tweet</button> 
+        <button type="button" class="btn btn-primary" style="width:160px">Tweet</button>
     </div>
 
     <div class="main">
